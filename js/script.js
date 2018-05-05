8a80d1a7058b2c1f06e03348a6bbdc7d71ff1585
Onur Kapçık <onurkapcik@gmail.com>
2018-05-05 12:22:05 +0300

    Init commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    'use strict';
+
+    function homepageResponsive() {
+
+        var windowsWidth = $(window).width(),
+            windowsHeight = $(window).height();
+
+        if (windowsWidth > windowsHeight) {
+
+            $('.introduction').css({
+                width: '100%',
+                height: '100%'
+            });
+
+        } else {
+
+            $('.introduction').css({
+                width: '100%',
+                height: '100%'
+            });
+
+        }
+
+        var introWidth = $('.introduction').width(),
+            introHeight = $('.introduction').height(),
+            bgImage = $('.introduction').find('img');
+
+        if (introWidth > introHeight) {
+
+            bgImage.css({
+                width: '100%',
+                height: '100%'
+            });
+
+        } else {
+
+            bgImage.css({
+                width: '100%',
+                height: '100%'
+            });
+
+        }
+    }
+
+    $(window).on('load resize', homepageResponsive);
+
+
+    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+});
